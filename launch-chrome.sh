0a212f25db3c8144748d23185ecb6c32a96a61c7
ragingwind <ragingwind@gmail.com>
2016-03-29 08:11:31 +0900

    Using npm script to run chrome instead of sh

--- launch-chrome.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-/Applications/Google\ Chrome\ Canary.app/Contents/MacOS/Google\ Chrome\ Canary  \
-  --remote-debugging-port=9222 \
-  --no-first-run \
-  --enable-gpu-benchmarking \
-  --user-data-dir="/tmp/lighthouse-profile" \
-  "about:blank"
-
