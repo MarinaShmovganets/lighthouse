34e5015797cf09f2098f981acf021d4a1524b495
Paul Irish <paul.irish@gmail.com>
2016-07-07 22:47:23 -0700

    ok

--- _install_deps.js
+++ _install_deps.js
@@ -1,16 +1,24 @@
 const exec = require('child_process').exec;
 
-var cwd = require('path').resolve()
-
+var cwd = require('path').resolve();
 const corePath = `${cwd}/lighthouse-core`;
 const extPath = `${cwd}/lighthouse-extension`;
-const cmd = `npm --prefix ${corePath} install ${corePath} && npm --prefix ${extPath} install ${extPath}`;
+
+const npm = 'npm --prefix';
+const cmd = `${npm} ${corePath} install ${corePath} && ${npm} ${extPath} install ${extPath}`;
 
 console.log(cmd);
 console.log('...');
 
-console.log(process.cwd());
-
-const child = exec(cmd);
+const child = exec(cmd,
+  function(error, stdout, stderr) {
+    process.stderr.write(stderr);
+    process.stdout.write(stdout);
+    if (stdout.length === 0) {
+      console.log('The full install may not have completed.');
+      console.log('To manually install child dependencies:');
+      console.log(`    ${cmd}`);
+    }
+  });
 child.stderr.pipe(process.stderr);
 child.stdout.pipe(process.stdout);
