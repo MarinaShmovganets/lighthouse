296587f81fcdd43aae86fecbab79bdad4da5d2a0
Paul Irish <paul.irish@gmail.com>
2016-07-08 13:37:31 -0700

    review feedback

--- _install_deps.js
+++ _install_deps.js
@@ -1,16 +1,36 @@
+/**
+ * @license
+ * Copyright 2016 Google Inc. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+'use strict';
+
 const exec = require('child_process').exec;
 
-var cwd = require('path').resolve();
+const cwd = require('path').resolve();
 const corePath = `${cwd}/lighthouse-core`;
 const extPath = `${cwd}/lighthouse-extension`;
 
 const npm = 'npm --prefix';
 const cmd = `${npm} ${corePath} install ${corePath} && ${npm} ${extPath} install ${extPath}`;
 
+// Tell the user what command we're about to execute
 console.log(cmd);
 console.log('...');
 
-const child = exec(cmd,
+exec(cmd,
   function(error, stdout, stderr) {
     process.stderr.write(stderr);
     process.stdout.write(stdout);
@@ -20,5 +40,4 @@ const child = exec(cmd,
       console.log(`    ${cmd}`);
     }
   });
-child.stderr.pipe(process.stderr);
-child.stdout.pipe(process.stdout);
+
