b3274e4371411baeb320037084839c8d2389d2f7
Eric Bidelman <ebidel@gmail.com>
2016-12-14 12:35:29 -0800

    Fail after lint error

--- lighthouse-viewer/gulpfile.js
+++ lighthouse-viewer/gulpfile.js
@@ -41,7 +41,8 @@ gulp.task('lint', () => {
     'gulpfile.js'
   ])
   .pipe($.eslint())
-  .pipe($.eslint.format());
+  .pipe($.eslint.format())
+  .pipe($.eslint.failAfterError());
 });
 
 gulp.task('images', () => {
