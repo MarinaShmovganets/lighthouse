9877a745096686d5900125fd6d1ca1ebcf68ac29
Jan Potoms <potoms.jan@gmail.com>
2017-01-10 12:18:23 +0100

    Make linter happy

--- lighthouse-extension/app/src/popup.js
+++ lighthouse-extension/app/src/popup.js
@@ -173,7 +173,7 @@ document.addEventListener('DOMContentLoaded', _ => {
       }
 
       if (err.code === 'PAGE_LOAD_ERROR') {
-        message = 'Unable to load the page. Please verify the url you are trying to review.'
+        message = 'Unable to load the page. Please verify the url you are trying to review.';
         includeReportLink = false;
       }
 
