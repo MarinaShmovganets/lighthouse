cd41a2468ab6300fb3ec6588e7e5803343241065
Eric Bidelman <ebidel@gmail.com>
2017-01-26 11:31:26 -0800

    Tests

--- lighthouse-cli/test/smokehouse/dobetterweb/dbw-expectations.js
+++ lighthouse-cli/test/smokehouse/dobetterweb/dbw-expectations.js
@@ -102,7 +102,9 @@ module.exports = [
         score: false,
         extendedInfo: {
           value: {
-            length: 7
+            results: {
+              length: 7
+            }
           }
         }
       },
