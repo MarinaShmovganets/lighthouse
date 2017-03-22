b35ac3cf71e36eba5e8e17c11d2d4670f64b7863
Paul Irish <paul.irish@gmail.com>
2017-03-22 12:17:39 -0700

    feedback.

--- lighthouse-core/audits/load-fast-enough-for-pwa.js
+++ lighthouse-core/audits/load-fast-enough-for-pwa.js
@@ -1,7 +1,18 @@
 /**
- * @license Copyright 2017 Google Inc. All Rights Reserved.
- * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License. You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
- * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.
+ * @license
+ * Copyright 2017 Google Inc. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 
 'use strict';
@@ -43,13 +54,14 @@ class LoadFastEnough4Pwa extends Audit {
   static audit(artifacts) {
     const networkRecords = artifacts.networkRecords[Audit.DEFAULT_PASS];
     const allRequestLatencies = networkRecords.map(record => {
-      if (!record._timing) return Infinity;
+      if (!record._timing) return undefined;
       // Use DevTools' definition of Waiting latency: https://github.com/ChromeDevTools/devtools-frontend/blob/66595b8a73a9c873ea7714205b828866630e9e82/front_end/network/RequestTimingView.js#L164
       return record._timing.receiveHeadersEnd - record._timing.sendEnd;
     });
 
     const latency3gMin = Emulation.settings.TYPICAL_MOBILE_THROTTLING_METRICS.latency - 10;
-    const areLatenciesAll3G = allRequestLatencies.every(val => val > latency3gMin);
+    const areLatenciesAll3G = allRequestLatencies.every(val =>
+        val === undefined || val > latency3gMin);
 
     return TTIMetric.audit(artifacts).then(ttiResult => {
       const timeToInteractive = ttiResult.extendedInfo.value.timings.timeToInteractive;
--- lighthouse-core/test/audits/load-fast-enough-for-pwa-test.js
+++ lighthouse-core/test/audits/load-fast-enough-for-pwa-test.js
@@ -1,7 +1,18 @@
 /**
- * @license Copyright 2017 Google Inc. All Rights Reserved.
- * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License. You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
- * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.
+ * @license
+ * Copyright 2017 Google Inc. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 
 'use strict';
@@ -47,8 +58,6 @@ describe('PWA: load-fast-enough-for-pwa audit', () => {
     TTIAudit.audit = generateTTIResults(5000);
     return FastPWAAudit.audit(generateArtifacts()).then(result => {
       assert.equal(result.rawValue, true, 'fixture trace is not passing audit');
-    }).catch(err => {
-      assert.ok(false, err);
     });
   });
 
@@ -57,8 +66,6 @@ describe('PWA: load-fast-enough-for-pwa audit', () => {
     return FastPWAAudit.audit(generateArtifacts()).then(result => {
       assert.equal(result.rawValue, false, 'not failing a long TTI value');
       assert.ok(result.debugString);
-    }).catch(err => {
-      assert.ok(false, err);
     });
   });
 
@@ -74,8 +81,6 @@ describe('PWA: load-fast-enough-for-pwa audit', () => {
     return FastPWAAudit.audit(generateArtifacts(mockNetworkRecords)).then(result => {
       assert.equal(result.rawValue, false);
       assert.ok(result.debugString.includes('network request latencies'));
-    }).catch(err => {
-      assert.ok(false, err);
     });
   });
 
@@ -92,8 +97,6 @@ describe('PWA: load-fast-enough-for-pwa audit', () => {
     return FastPWAAudit.audit(generateArtifacts(mockNetworkRecords)).then(result => {
       assert.equal(result.rawValue, true);
       assert.strictEqual(result.debugString, undefined);
-    }).catch(err => {
-      assert.ok(false, err);
     });
   });
 });
