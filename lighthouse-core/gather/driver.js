558109695965ddb1a48752d354104b2dbd5703b8
Brendan Kenny <bckenny@google.com>
2017-05-01 16:23:08 -0700

    handle all javascript dialogs
    also add a window.confirm to dbw_tester

--- lighthouse-core/gather/driver.js
+++ lighthouse-core/gather/driver.js
@@ -841,7 +841,7 @@ class Driver {
    */
   dismissJavaScriptDialogs() {
     return this.sendCommand('Page.enable').then(_ => {
-      this.once('Page.javascriptDialogOpening', _ => {
+      this.on('Page.javascriptDialogOpening', _ => {
         // rejection intentionally unhandled
         this.sendCommand('Page.handleJavaScriptDialog', {
           accept: true,
