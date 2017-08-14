bec47c3b879870b6485b5d19b6a2f621b3fd5039
Paul Irish <paul.irish@gmail.com>
2017-08-14 14:29:20 -0700

    Adjusted wording

--- lighthouse-core/audits/webapp-install-banner.js
+++ lighthouse-core/audits/webapp-install-banner.js
@@ -81,7 +81,7 @@ class WebappInstallBanner extends MultiCheckAudit {
     const hasOfflineStartUrl = artifacts.StartUrl.statusCode === 200;
 
     if (!hasOfflineStartUrl) {
-      result.failures.push('Manifest start_url is not cached and returned by a service worker');
+      result.failures.push('Service worker does not successfully serve the manifest\'s start_url');
     }
 
     if (artifacts.StartUrl.debugString) {
