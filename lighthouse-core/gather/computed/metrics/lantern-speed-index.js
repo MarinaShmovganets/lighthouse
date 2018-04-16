cf0417c6173418ca9e1ea66d8dd260cd199ffcc5
Patrick Hulce <patrick.hulce@gmail.com>
2018-04-16 12:44:32 -0700

    add explanation and link to doc

--- lighthouse-core/gather/computed/metrics/lantern-speed-index.js
+++ lighthouse-core/gather/computed/metrics/lantern-speed-index.js
@@ -74,6 +74,16 @@ class SpeedIndex extends MetricArtifact {
   }
 
   /**
+   * Approximate speed index using layout events from the simulated node timings.
+   * The layout-based speed index is the weighted average of the endTime of CPU nodes that contained
+   * a 'Layout' task. log(duration) is used as the weight to stand for "significance" to the page.
+   *
+   * If no layout events can be found or the endTime of a CPU task is too early, FCP is used instead.
+   *
+   * This approach was determined after evaluating the accuracy/complexity tradeoff of many
+   * different methods. Read more in the evaluation doc.
+   * @see https://docs.google.com/document/d/1qJWXwxoyVLVadezIp_Tgdk867G3tDNkkVRvUJSH3K1E/edit#
+   *
    * @param {Map<Node, LH.Gatherer.Simulation.NodeTiming>} nodeTiming
    * @param {number} fcpTimeInMs
    * @return {number}
