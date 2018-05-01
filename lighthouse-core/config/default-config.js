94f5155691ee1bc90d4d56de1929cdf8dc5b4c1c
Patrick Hulce <patrick.hulce@gmail.com>
2018-04-30 17:43:34 -0700

    bump to 1000

--- lighthouse-core/config/default-config.js
+++ lighthouse-core/config/default-config.js
@@ -15,9 +15,9 @@ module.exports = {
     passName: 'defaultPass',
     recordTrace: true,
     useThrottling: true,
-    pauseAfterLoadMs: 500,
-    networkQuietThresholdMs: 500,
-    cpuQuietThresholdMs: 500,
+    pauseAfterLoadMs: 1000,
+    networkQuietThresholdMs: 1000,
+    cpuQuietThresholdMs: 1000,
     gatherers: [
       'url',
       'scripts',
