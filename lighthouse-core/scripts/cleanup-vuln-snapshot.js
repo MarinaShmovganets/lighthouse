d4df09d64ff671e993967e8c0de09e05b610144c
Paul Irish <paul.irish@gmail.com>
2018-05-18 11:13:02 -0700

    adopt pretty-json-stringify w/ types

--- lighthouse-core/scripts/cleanup-vuln-snapshot.js
+++ lighthouse-core/scripts/cleanup-vuln-snapshot.js
@@ -10,6 +10,7 @@
 /** @fileoverview Read in the snyk snapshot, remove whatever we don't need, write it back */
 
 const {readFileSync, writeFileSync} = require('fs');
+const prettyJSONStringify = require('pretty-json-stringify');
 
 const filename = process.argv[2];
 if (!filename) throw new Error('No filename provided.');
@@ -19,12 +20,14 @@ const output = cleanAndFormat(data);
 JSON.parse(output); // make sure it's parseable
 writeFileSync(filename, output, 'utf8');
 
+/** @typedef {import('../audits/dobetterweb/no-vulnerable-libraries.js').SnykDB} SnykDB */
+
 /**
  * @param {string} vulnString
  * @return {string}
  */
 function cleanAndFormat(vulnString) {
-  const snapshot = JSON.parse(vulnString);
+  const snapshot = /** @type {!SnykDB} */ (JSON.parse(vulnString));
   for (const libEntries of Object.values(snapshot.npm)) {
     libEntries.forEach((entry, i) => {
       const pruned = {
@@ -33,13 +36,17 @@ function cleanAndFormat(vulnString) {
         semver: {vulnerable: entry.semver.vulnerable},
       };
 
-      libEntries[i] = `SUPERHACKB${JSON.stringify(pruned)}SUPERHACKN`;
+      libEntries[i] = pruned;
     });
   }
 
-  const out = JSON.stringify(snapshot, null, 2);
-  return out.replace(/"SUPERHACKB(.*?)SUPERHACKN"/g, function(_, s) {
-    const orig = JSON.parse(JSON.parse(`"${s}"`));
-    return JSON.stringify(orig);
+  // Normal pretty JSON-stringify has too many newlines. This strikes the right signal:noise ratio
+  return prettyJSONStringify(snapshot, {
+    tab: '  ',
+    spaceBeforeColon: '',
+    spaceAfterColon: '',
+    spaceAfterComma: '',
+    spaceInsideObject: '',
+    shouldExpand: (_, level) => level < 3,
   });
 }
