f6080e814bf9eb5d10a4b116de12a7edf0c020bc
webcarrot <admin@webcarrot.pl>
2018-06-05 10:43:08 +0200

    Fix for linter

--- lighthouse-core/gather/gatherers/dobetterweb/all-event-listeners.js
+++ lighthouse-core/gather/gatherers/dobetterweb/all-event-listeners.js
@@ -59,7 +59,7 @@ class EventListeners extends Gatherer {
       .catch(() => {
         return {
           objectId: null,
-          description: ""
+          description: ''
         };
       });
     }
