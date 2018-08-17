be98c42fcb27bd91e5846df66ca2accacd0be2fc
Patrick Hulce <patrick.hulce@gmail.com>
2018-08-17 13:28:26 -0700

    core: enhanced noopener output

--- lighthouse-cli/test/smokehouse/dobetterweb/dbw-expectations.js
+++ lighthouse-cli/test/smokehouse/dobetterweb/dbw-expectations.js
@@ -39,7 +39,7 @@ module.exports = [
       },
       'external-anchors-use-rel-noopener': {
         score: 0,
-        warnings: [/Unable to determine/],
+        warnings: [/Unable to determine.*<a target="_blank">external link/],
         details: {
           items: {
             length: 3,
