33d66666744ff8b43618ef7d86aa28638db278db
Paul Irish <paulirish@google.com>
2018-10-16 19:55:20 -0700

    core(driver): stringify protocol error messages

--- lighthouse-core/lib/lh-error.js
+++ lighthouse-core/lib/lh-error.js
@@ -59,7 +59,7 @@ class LighthouseError extends Error {
     }
 
     // otherwise fallback to building a generic Error
-    let errMsg = `(${method}): ${protocolError.message}`;
+    let errMsg = `(${method}): ${JSON.stringify(protocolError.message)}`;
     if (protocolError.data) errMsg += ` (${protocolError.data})`;
     const error = new Error(`Protocol error ${errMsg}`);
     return Object.assign(error, {protocolMethod: method, protocolError: protocolError.message});
