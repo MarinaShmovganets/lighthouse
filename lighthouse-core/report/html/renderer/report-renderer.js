b76d771cfcdd68ee7a52d49e2699d61194a29b47
Connor Clark <cjamcl@google.com>
2019-01-24 17:00:10 -0500

    k

--- lighthouse-core/report/html/renderer/report-renderer.js
+++ lighthouse-core/report/html/renderer/report-renderer.js
@@ -207,14 +207,12 @@ class ReportRenderer {
     }
 
     // Fireworks
-    if (!this._dom.isDevTools()) {
-      const scoresAll100 = report.reportCategories.every(cat => cat.score === 1);
-      if (scoresAll100) {
-        headerContainer.classList.add('score100');
-        this._dom.find('.lh-header', headerContainer).addEventListener('click', _ => {
-          headerContainer.classList.toggle('fireworks-paused');
-        });
-      }
+    const scoresAll100 = report.reportCategories.every(cat => cat.score === 1);
+    if (!this._dom.isDevTools() && scoresAll100) {
+      headerContainer.classList.add('score100');
+      this._dom.find('.lh-header', headerContainer).addEventListener('click', _ => {
+        headerContainer.classList.toggle('fireworks-paused');
+      });
     }
 
     if (scoreHeader) {
