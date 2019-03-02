83c043e96a655c3ad8971e802205d96b4b268468
Patrick Hulce <patrick.hulce@gmail.com>
2019-03-02 10:38:36 -0600

    lower to 15s

--- lighthouse-core/config/constants.js
+++ lighthouse-core/config/constants.js
@@ -42,7 +42,7 @@ const throttling = {
 /** @type {LH.Config.Settings} */
 const defaultSettings = {
   output: 'json',
-  maxWaitForFcp: 30 * 1000,
+  maxWaitForFcp: 15 * 1000,
   maxWaitForLoad: 45 * 1000,
   throttlingMethod: 'simulate',
   throttling: throttling.mobileSlow4G,
