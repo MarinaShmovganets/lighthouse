9c6e028721477da069e76adfd9c5d685b9a14cd3
Paul Irish <paulirish@google.com>
2019-04-11 14:47:22 -0700

    feedback

--- .build-for-now.sh
+++ .build-for-now.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 set -x
 
-# manually install this one report dependency. Avoid installing all deps.
+# Manually install this one report dependency. Avoid installing all deps (for speed)
 rm package.json
 npm install details-element-polyfill@2.2.0
 
-# create dist if it's not already there
+# Create dist if it's not already there
 mkdir -p dist
 
-# generate the report and place as dist/index.html
+# Generate the report and place as dist/index.html
 node lighthouse-core/scripts/build-report-to-dist.js
