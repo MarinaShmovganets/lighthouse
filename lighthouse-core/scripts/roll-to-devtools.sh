c8da095cb06e513fa06d2dae37e56a2fa32ae9d6
Connor Clark <cjamcl@google.com>
2019-05-17 15:35:14 -0700

    echo command to rebase tests

--- lighthouse-core/scripts/roll-to-devtools.sh
+++ lighthouse-core/scripts/roll-to-devtools.sh
@@ -48,3 +48,5 @@ cp -r dist/dt-report-resources/ $fe_lh_dir
 # update expected version string in tests
 VERSION=$(node -e "console.log(require('./package.json').version)")
 sed -i '' -e "s/Version:.*/Version: $VERSION/g" "$tests_dir"/*-expected.txt
+
+echo "Done. To rebase the test expectations, run: ~/chromium/src/third_party/blink/tools/run_web_tests.py --no-retry 'http/tests/devtools/audits2/*' --reset-results"
