4653ba138ae54e9d46eaa995fc4042a8c35671e9
Connor Clark <cjamcl@google.com>
2019-07-10 14:27:26 -0700

    update

--- lighthouse-core/test/audits/dobetterweb/no-vulnerable-libraries-test.js
+++ lighthouse-core/test/audits/dobetterweb/no-vulnerable-libraries-test.js
@@ -101,40 +101,54 @@ describe('Avoids front-end JavaScript libraries with known vulnerabilities', () 
   });
 });
 
-// https://github.com/npm/node-semver/issues/166#issuecomment-245990039
-function hasUpperBound(rangeString) {
-  const range = new semver.Range(rangeString);
-  if (!range) return false;
+describe('Snyk database', () => {
+  // https://github.com/npm/node-semver/issues/166#issuecomment-245990039
+  function hasUpperBound(rangeString) {
+    const range = new semver.Range(rangeString);
+    if (!range) return false;
 
-  // For every subset ...
-  for (const subset of range.set) {
+    // For every subset ...
+    for (const subset of range.set) {
     // Upperbound exists if...
 
     // < or <= is in one of the subset's clauses (= gets normalized to >= and <).
-    if (subset.some(comparator => comparator.operator.match(/^</))) {
-      continue;
+      if (subset.some(comparator => comparator.operator && comparator.operator.match(/^</))) {
+        continue;
+      }
+
+      // Subset has a prerelease tag (operator will be empty string).
+      if (subset.length === 1 && subset[0].operator === '') {
+        continue;
+      }
+
+      // No upperbound for this subset.
+      return false;
     }
 
-    // Subset has a prerelease tag (operator will be empty).
-    if (subset.length === 1 && subset[0].operator === '') {
-      continue;
-    }
-
-    // No upperbound for this subset.
-    return false;
+    return true;
   }
 
-  return true;
-}
+  it('hasUpperBound works as intended', () => {
+    assert.equal(hasUpperBound('<1.12.2'), true);
+    assert.equal(hasUpperBound('=1.12.2'), true);
+    assert.equal(hasUpperBound('>=1.12.3 <2.2.2'), true);
+    assert.equal(hasUpperBound('>=2.2.3 <3.0.0'), true);
+    assert.equal(hasUpperBound('>=3.0.0 <3.10.1 || =3.10.2'), true);
 
-describe('every snyk vulnerability has an upper bound', () => {
-  for (const vulns of Object.values(NoVulnerableLibrariesAudit.snykDB.npm)) {
-    for (const vuln of vulns) {
-      for (const semver of vuln.semver.vulnerable) {
-        if (!hasUpperBound(semver)) {
-          assert.fail(`invalid semver: ${semver}. Must contain an upper bound`);
+    assert.equal(hasUpperBound('>1.12.2'), false);
+    assert.equal(hasUpperBound('>=1.12.2'), false);
+    assert.equal(hasUpperBound('*'), false);
+  });
+
+  it('every snyk vulnerability has an upper bound', () => {
+    for (const vulns of Object.values(NoVulnerableLibrariesAudit.snykDB.npm)) {
+      for (const vuln of vulns) {
+        for (const semver of vuln.semver.vulnerable) {
+          if (!hasUpperBound(semver)) {
+            assert.fail(`invalid semver: ${semver}. Must contain an upper bound`);
+          }
         }
       }
     }
-  }
+  });
 });
