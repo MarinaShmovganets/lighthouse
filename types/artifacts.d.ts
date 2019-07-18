c2f840316fb362490eefd7a09e91f48c9e00d94a
Connor Clark <cjamcl@google.com>
2019-07-18 12:02:14 -0700

    words

--- types/artifacts.d.ts
+++ types/artifacts.d.ts
@@ -241,7 +241,7 @@ declare global {
         failingTextLength: number;
         visitedTextLength: number;
         analyzedFailingTextLength: number;
-        /** Elements with a text node that failed size criteria. */
+        /** Elements that contain a text node that failed size criteria. */
         analyzedFailingNodesData: Array<{
           fontSize: number;
           textLength: number;
