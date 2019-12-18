39b5f6c5ea2598324f103c8df71880ae79dfbf65
Connor Clark <cjamcl@google.com>
2019-12-18 11:26:04 -0800

    fix logic

--- lighthouse-core/scripts/lantern/download-traces.sh
+++ lighthouse-core/scripts/lantern/download-traces.sh
@@ -10,7 +10,7 @@ DIRNAME="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 LH_ROOT_PATH="$DIRNAME/../../.."
 cd $LH_ROOT_PATH
 
-if [[ -f lantern-data/version ]] && [["$VERSION" == "$(cat lantern-data/version)"]]; then
+if [[ -f lantern-data/version ]] && [["$VERSION" != "$(cat lantern-data/version)"]]; then
   echo "Deleting old lantern data."
   rm -rf lantern-data/
 fi
