b56fcd0a0a649f09ffeb2c0a8bfe69d8016dc898
James Addison <james@reciperadar.com>
2019-12-19 15:43:23 +0000

    Add TODO pending correct error handling

--- lighthouse-core/audits/accessibility/axe-audit.js
+++ lighthouse-core/audits/accessibility/axe-audit.js
@@ -44,7 +44,8 @@ class AxeAudit extends Audit {
     // If aXe indicates an incomplete result with no error & no matching elements, pass the audit
     const incomplete = artifacts.Accessibility.incomplete || [];
     const isIncomplete = incomplete.find(result => result.id === this.meta.id);
-    if (isIncomplete && !isIncomplete.error && (isIncomplete.nodes || []).length === 0) {
+    // TODO: Check error cases here
+    if (isIncomplete && (isIncomplete.nodes || []).length === 0) {
       return {
         score: 1,
       };
