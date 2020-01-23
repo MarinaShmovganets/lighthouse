6bd8740c7ff55142a4058ad0a7a8795125da9f7f
Connor Clark <cjamcl@google.com>
2020-01-23 12:53:53 -0800

    fix

--- build/build-pack.sh
+++ build/build-pack.sh
@@ -5,8 +5,8 @@ DIRNAME="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 tmp_dir=$(mktemp -d -t lh-XXXXXXXXXX)
 lh_dir="$DIRNAME/.."
 
-cd tmp_dir
+cd "$tmp_dir"
 npm pack "$lh_dir"
 mv *.tgz "$lh_dir/dist/lighthouse.tgz"
 
-rmdir $tmp_dir
+rmdir "$tmp_dir"
