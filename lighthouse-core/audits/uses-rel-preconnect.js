eca7de8a42abd2f53f8fdec4b0e8c1b8df6df818
Piotr Zarycki <piotr.zarycki@gmail.com>
2020-01-29 12:52:28 +0100

    core(audits): stop suggesting origin CR fix

--- lighthouse-core/audits/uses-rel-preconnect.js
+++ lighthouse-core/audits/uses-rel-preconnect.js
@@ -35,9 +35,6 @@ const UIStrings = {
    * */
   crossoriginWarning: 'A preconnect <link> was found for "{securityOrigin}" but was not used ' +
     'by the browser. Check that you are using the `crossorigin` attribute properly.',
-  /**
-   * @description A warning message that is shown when found more than 3 preconnected links
-   * */
   tooManyPreconnectLinksWarning: 'More than 2 preconnect links were found. ' +
    'Preconnect links should be used sparingly and only to the most important origins.',
 };
