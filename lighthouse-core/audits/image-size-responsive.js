99e85ebfa5e0a7dd458de4134ec6219a1465a3f5
Sebastian Kreft <skreft@gmail.com>
2020-03-13 18:37:08 -0300

    new_audit: check images are big enough.
    New audit checking images are big enough. This is a complement to UsesResponsiveImages.
    
    Fixes #10434.

---
+++ lighthouse-core/audits/image-size-responsive.js
@@ -0,0 +1,204 @@
+/**
+ * @license Copyright 2020 Sebastian Kreft All Rights Reserved.
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License. You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.
+ */
+/**
+ * @fileoverview Checks to see if the aspect ratio of the images used on
+ *   the page are equal to the aspect ratio of their display sizes. The
+ *   audit will list all images that don't match with their display size
+ *   aspect ratio.
+ */
+'use strict';
+
+const Audit = require('./audit.js');
+const URL = require('../lib/url-shim.js');
+const i18n = require('../lib/i18n/i18n.js');
+
+const UIStrings = {
+  /** Title of a Lighthouse audit that provides detail on the size of visible images on the page. This descriptive title is shown to users when all images have correct sizes. */
+  title: 'Displays images with correct size',
+  /** Title of a Lighthouse audit that provides detail on the size of visible images on the page. This descriptive title is shown to users when not all images have correct sizes. */
+  failureTitle: 'Displays images with incorrect size',
+  /** Description of a Lighthouse audit that tells the user why they should maintain the correct aspect ratios for all images. This is displayed after a user expands the section to see more. No character length limits. 'Learn More' becomes link text to additional documentation. */
+  description: 'Image display dimensions should match natural aspect ratio. ' +
+    '[Learn more](https://web.dev/image-aspect-ratio).',
+  /**
+   * @description Warning that the size information for an image was nonsensical.
+   * @example {https://image.cdn.com/} url
+   */
+  warningCompute: 'Invalid image sizing information {url}',
+  /**  Label for a column in a data table; entries in the column will be the numeric aspect ratio of an image as displayed in a web page. */
+  columnDisplayed: 'Displayed size',
+  /**  Label for a column in a data table; entries in the column will be the numeric aspect ratio of the raw (actual) image. */
+  columnActual: 'Actual size',
+  /**  Label for a column in a data table; entries in the column will be the numeric aspect ratio of the raw (actual) image. */
+  columnExpected: 'Expected size',
+};
+
+const str_ = i18n.createMessageInstanceIdFn(__filename, UIStrings);
+
+const TOLERANCE = 0.75;
+
+const ALLOWABLE_OFFSCREEN_X = 100;
+const ALLOWABLE_OFFSCREEN_Y = 200;
+
+/** @typedef {{url: string, elidedUrl: string, displayedSize: string, actualSize: string, expectedSize: string, expectedPixels: number}} Result */
+
+/**
+ * @param {{top: number, bottom: number, left: number, right: number}} imageRect
+ * @param {{innerWidth: number, innerHeight: number}} viewportDimensions
+ * @return {boolean}
+ */
+function isVisible(imageRect, viewportDimensions) {
+  return (
+    (imageRect.bottom - imageRect.top) * (imageRect.right - imageRect.left) > 0 &&
+    imageRect.top <= viewportDimensions.innerHeight + ALLOWABLE_OFFSCREEN_Y &&
+    imageRect.bottom >= -ALLOWABLE_OFFSCREEN_Y &&
+    imageRect.left <= viewportDimensions.innerWidth + ALLOWABLE_OFFSCREEN_X &&
+    imageRect.right >= -ALLOWABLE_OFFSCREEN_X
+  );
+}
+
+/**
+ * @param {LH.Artifacts.ImageElement} image
+ * @return {boolean}
+ */
+function isCandidate(image) {
+  if (image.displayedWidth <= 1 || image.displayedHeight <= 1) {
+    return false;
+  }
+  if (image.naturalWidth === 0 || image.naturalHeight === 0) {
+    return false;
+  }
+  if (image.mimeType === 'image/svg+xml') {
+    return false;
+  }
+  if (image.isCss) {
+    return false;
+  }
+  if (image.usesObjectFit) {
+    return false;
+  }
+  return true;
+}
+
+/**
+ * @param {LH.Artifacts.ImageElement} image
+ * @param {number} DPR
+ * @return {boolean}
+ */
+function imageHasRightSize(image, DPR) {
+  const [expectedWidth, expectedHeight] =
+      expectedImageSize(image.displayedWidth, image.displayedHeight, DPR);
+  return image.naturalWidth >= expectedWidth && image.naturalHeight >= expectedHeight;
+}
+
+/**
+ * @param {LH.Artifacts.ImageElement} image
+ * @param {number} DPR
+ * @return {Result}
+ */
+function getResult(image, DPR) {
+  const [expectedWidth, expectedHeight] =
+      expectedImageSize(image.displayedWidth, image.displayedHeight, DPR);
+  return {
+    url: image.src,
+    elidedUrl: URL.elideDataURI(image.src),
+    displayedSize: `${image.displayedWidth} x ${image.displayedHeight}`,
+    actualSize: `${image.naturalWidth} x ${image.naturalHeight}`,
+    expectedSize: `${expectedWidth} x ${expectedHeight}`,
+    expectedPixels: expectedWidth * expectedHeight,
+  };
+}
+
+/**
+ * Compute the size an image should have given the display dimensions and pixel density.
+ *
+ * For smaller images, typically icons, the size must be proportional to the density.
+ * For larger images some tolerance is allowed as in thise cases the perceived degradation is not
+ * that bad.
+ *
+ * @param {number} displayedWidth
+ * @param {number} displayedHeight
+ * @param {number} DPR
+ * @return {[number, number]}
+ */
+function expectedImageSize(displayedWidth, displayedHeight, DPR) {
+  let factor = DPR;
+  if (displayedWidth > 64 || displayedHeight > 64) {
+    factor *= TOLERANCE;
+  }
+  const width = Math.ceil(factor * displayedWidth);
+  const height = Math.ceil(factor * displayedHeight);
+  return [width, height];
+}
+
+/**
+ * Remove repeated entries for the same source and sort them by source.
+ *
+ * It will keep the entry with the largest size.
+ *
+ * @param {Result[]} results
+ * @return {Result[]}
+ */
+function deduplicateAndSortResults(results) {
+  results.sort((a, b) => a.url === b.url ? 0 : (a.url < b. url ? -1 : 1));
+  const deduplicated = /** @type {Result[]} */ ([]);
+  for (const r of results) {
+    if (deduplicated.length > 0 && r.url === deduplicated[deduplicated.length - 1].url) {
+      if (deduplicated[deduplicated.length - 1].expectedPixels < r.expectedPixels) {
+        deduplicated[deduplicated.length - 1] = r;
+      }
+    } else {
+      deduplicated.push(r);
+    }
+  }
+  return deduplicated;
+}
+
+class ImageSizeResponsive extends Audit {
+  /**
+   * @return {LH.Audit.Meta}
+   */
+  static get meta() {
+    return {
+      id: 'image-size-responsive',
+      title: str_(UIStrings.title),
+      failureTitle: str_(UIStrings.failureTitle),
+      description: str_(UIStrings.description),
+      requiredArtifacts: ['ImageElements', 'ViewportDimensions'],
+    };
+  }
+
+  /**
+   * @param {LH.Artifacts} artifacts
+   * @return {LH.Audit.Product}
+   */
+  static audit(artifacts) {
+    const DPR = artifacts.ViewportDimensions.devicePixelRatio;
+    const results = Array
+      .from(artifacts.ImageElements)
+      .filter(isCandidate)
+      .filter(image => !imageHasRightSize(image, DPR))
+      .filter(image => isVisible(image.clientRect, artifacts.ViewportDimensions))
+      .map(image => getResult(image, DPR));
+
+    /** @type {LH.Audit.Details.Table['headings']} */
+    const headings = [
+      {key: 'url', itemType: 'thumbnail', text: ''},
+      {key: 'elidedUrl', itemType: 'url', text: str_(i18n.UIStrings.columnURL)},
+      {key: 'displayedSize', itemType: 'text', text: str_(UIStrings.columnDisplayed)},
+      {key: 'actualSize', itemType: 'text', text: str_(UIStrings.columnActual)},
+      {key: 'expectedSize', itemType: 'text', text: str_(UIStrings.columnExpected)},
+    ];
+
+    return {
+      score: Number(results.length === 0),
+      details: Audit.makeTableDetails(headings, deduplicateAndSortResults(results)),
+    };
+  }
+}
+
+module.exports = ImageSizeResponsive;
+module.exports.UIStrings = UIStrings;
---
+++ lighthouse-core/test/audits/image-size-responsive-test.js
@@ -0,0 +1,363 @@
+/**
+ * @license Copyright 2020 Sebastian Kreft All Rights Reserved.
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License. You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.
+ */
+'use strict';
+
+const ImageSizeResponsiveAudit = require('../../audits/image-size-responsive.js');
+const assert = require('assert');
+
+/* eslint-env jest */
+
+const WIDTH = 800;
+const HEIGHT = 600;
+
+function generateImage(clientSize, naturalSize, props, src = 'https://google.com/logo.png') {
+  const image = {src, mimeType: 'image/png'};
+  const clientRect = {
+    clientRect: {
+      top: 0,
+      bottom: clientSize.displayedHeight,
+      left: 0,
+      right: clientSize.displayedWidth,
+    },
+  };
+  Object.assign(image, clientSize, naturalSize, clientRect, props);
+  return image;
+}
+
+describe('Images: size audit', () => {
+  function testImage(condition, data) {
+    const description = `identifies when an image ${condition}`;
+    it(description, () => {
+      const result = ImageSizeResponsiveAudit.audit({
+        ImageElements: [
+          generateImage(
+            {displayedWidth: data.clientSize[0], displayedHeight: data.clientSize[1]},
+            {naturalWidth: data.naturalSize[0], naturalHeight: data.naturalSize[1]},
+            data.props
+          ),
+        ],
+        ViewportDimensions: {
+          innerWidth: WIDTH,
+          innerHeight: HEIGHT,
+          devicePixelRatio: data.devicePixelRatio || 1,
+        },
+      });
+      let details = '';
+      if (result.score === 0) {
+        const {displayedSize: displayed, actualSize: actual, expectedSize: expected} =
+            result.details.items[0];
+        details = ` (displayed: ${displayed}, actual: ${actual}, expected: ${expected})`;
+      }
+      assert.strictEqual(result.score, data.score, `score does not match${details}`);
+    });
+  }
+
+  testImage('invalid image', {
+    score: 0,
+    clientSize: [100, 100],
+    naturalSize: [5, 5],
+  });
+
+  describe('is empty', () => {
+    testImage('is empty along width', {
+      score: 1,
+      clientSize: [100, 100],
+      naturalSize: [0, 5],
+    });
+
+    testImage('is empty along height', {
+      score: 1,
+      clientSize: [100, 100],
+      naturalSize: [5, 0],
+    });
+  });
+
+  describe('too small', () => {
+    testImage('is too small along width', {
+      score: 1,
+      clientSize: [1, 100],
+      naturalSize: [5, 5],
+    });
+
+    testImage('is too small along height', {
+      score: 1,
+      clientSize: [100, 1],
+      naturalSize: [5, 5],
+    });
+  });
+
+  testImage('is an SVG image', {
+    score: 1,
+    clientSize: [100, 100],
+    naturalSize: [5, 5],
+    props: {
+      mimeType: 'image/svg+xml',
+    },
+  });
+
+  testImage('is a css image', {
+    score: 1,
+    clientSize: [100, 100],
+    naturalSize: [5, 5],
+    props: {
+      isCss: true,
+    },
+  });
+
+  testImage('uses object-fit', {
+    score: 1,
+    clientSize: [100, 100],
+    naturalSize: [5, 5],
+    props: {
+      usesObjectFit: true,
+    },
+  });
+
+  describe('visibility', () => {
+    testImage('has no client area', {
+      score: 1,
+      clientSize: [100, 100],
+      naturalSize: [5, 5],
+      props: {
+        clientRect: {
+          top: 0,
+          bottom: 0,
+          left: 0,
+          right: 0,
+        },
+      },
+    });
+
+    testImage('is above the visible area', {
+      score: 1,
+      clientSize: [100, 100],
+      naturalSize: [5, 5],
+      props: {
+        clientRect: {
+          top: -201 - 100,
+          bottom: -201,
+          left: 0,
+          right: 100,
+        },
+      },
+    });
+
+    testImage('is almost above the visible area', {
+      score: 0,
+      clientSize: [100, 100],
+      naturalSize: [5, 5],
+      props: {
+        clientRect: {
+          top: -200 - 100,
+          bottom: -200,
+          left: 0,
+          right: 100,
+        },
+      },
+    });
+
+    testImage('is below the visible area', {
+      score: 1,
+      clientSize: [100, 100],
+      naturalSize: [5, 5],
+      props: {
+        clientRect: {
+          top: HEIGHT + 201,
+          bottom: HEIGHT + 201 + 100,
+          left: 0,
+          right: 100,
+        },
+      },
+    });
+
+    testImage('is almost below the visible area', {
+      score: 0,
+      clientSize: [100, 100],
+      naturalSize: [5, 5],
+      props: {
+        clientRect: {
+          top: HEIGHT + 200,
+          bottom: HEIGHT + 200 + 100,
+          left: 0,
+          right: 100,
+        },
+      },
+    });
+
+    testImage('is to the left of the visible area', {
+      score: 1,
+      clientSize: [100, 100],
+      naturalSize: [5, 5],
+      props: {
+        clientRect: {
+          top: 0,
+          bottom: 100,
+          left: -101 - 100,
+          right: -101,
+        },
+      },
+    });
+
+    testImage('is almost to the left of the visible area', {
+      score: 0,
+      clientSize: [100, 100],
+      naturalSize: [5, 5],
+      props: {
+        clientRect: {
+          top: 0,
+          bottom: 100,
+          left: -100 - 100,
+          right: -100,
+        },
+      },
+    });
+
+    testImage('is to the right of the visible area', {
+      score: 1,
+      clientSize: [100, 100],
+      naturalSize: [5, 5],
+      props: {
+        clientRect: {
+          top: 0,
+          bottom: 100,
+          left: WIDTH + 101,
+          right: WIDTH + 101 + 100,
+        },
+      },
+    });
+
+    testImage('is almost to the right of the visible area', {
+      score: 0,
+      clientSize: [100, 100],
+      naturalSize: [5, 5],
+      props: {
+        clientRect: {
+          top: 0,
+          bottom: 100,
+          left: WIDTH + 100,
+          right: WIDTH + 100 + 100,
+        },
+      },
+    });
+  });
+
+  describe('check size', () => {
+    describe('DPR = 1', () => {
+      testImage('is an icon with right size', {
+        score: 1,
+        clientSize: [64, 64],
+        naturalSize: [64, 64],
+      });
+
+      testImage('is an icon with an invalid size', {
+        score: 0,
+        clientSize: [64, 64],
+        naturalSize: [63, 63],
+      });
+
+      testImage('has right size', {
+        score: 1,
+        clientSize: [65, 65],
+        naturalSize: [49, 49],
+      });
+
+      testImage('has an invalid size', {
+        score: 0,
+        clientSize: [65, 65],
+        naturalSize: [48, 48],
+      });
+    });
+
+    describe('DPR = 2', () => {
+      testImage('is an icon with right size', {
+        score: 1,
+        clientSize: [64, 64],
+        naturalSize: [128, 128],
+        devicePixelRatio: 2,
+      });
+
+      testImage('is an icon with an invalid size', {
+        score: 0,
+        clientSize: [64, 64],
+        naturalSize: [127, 127],
+        devicePixelRatio: 2,
+      });
+
+      testImage('has right size', {
+        score: 1,
+        clientSize: [65, 65],
+        naturalSize: [98, 98],
+        devicePixelRatio: 2,
+      });
+
+      testImage('has an invalid size', {
+        score: 0,
+        clientSize: [65, 65],
+        naturalSize: [97, 97],
+        devicePixelRatio: 2,
+      });
+    });
+  });
+
+  it('de-dupes images', () => {
+    const result = ImageSizeResponsiveAudit.audit({
+      ImageElements: [
+        generateImage(
+          {displayedWidth: 80, displayedHeight: 40},
+          {naturalWidth: 40, naturalHeight: 20}
+        ),
+        generateImage(
+          {displayedWidth: 160, displayedHeight: 80},
+          {naturalWidth: 40, naturalHeight: 20}
+        ),
+        generateImage(
+          {displayedWidth: 60, displayedHeight: 30},
+          {naturalWidth: 40, naturalHeight: 20}
+        ),
+      ],
+      ViewportDimensions: {
+        innerWidth: WIDTH,
+        innerHeight: HEIGHT,
+        devicePixelRatio: 1,
+      },
+    });
+    assert.equal(result.details.items.length, 1);
+    assert.equal(result.details.items[0].expectedSize, '120 x 60');
+  });
+
+  it('sorts images', () => {
+    const result = ImageSizeResponsiveAudit.audit({
+      ImageElements: [
+        generateImage(
+          {displayedWidth: 80, displayedHeight: 40},
+          {naturalWidth: 40, naturalHeight: 20},
+          {},
+          'https://C.com/image.png'
+        ),
+        generateImage(
+          {displayedWidth: 80, displayedHeight: 40},
+          {naturalWidth: 40, naturalHeight: 20},
+          {},
+          'https://A.com/image.png'
+        ),
+        generateImage(
+          {displayedWidth: 80, displayedHeight: 40},
+          {naturalWidth: 40, naturalHeight: 20},
+          {},
+          'https://B.com/image.png'
+        ),
+      ],
+      ViewportDimensions: {
+        innerWidth: WIDTH,
+        innerHeight: HEIGHT,
+        devicePixelRatio: 1,
+      },
+    });
+    assert.equal(result.details.items.length, 3);
+    const srcs = result.details.items.map(item => item.url);
+    assert.deepEqual(Array.from(srcs), srcs.sort());
+  });
+});
