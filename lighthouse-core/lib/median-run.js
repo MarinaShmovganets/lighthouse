6a17c2cee38704ebb884bcee2180728d13f32b31
Patrick Hulce <patrick.hulce@gmail.com>
2020-05-28 14:47:07 -0500

    brendan feedback

--- lighthouse-core/lib/median-run.js
+++ lighthouse-core/lib/median-run.js
@@ -10,12 +10,25 @@ const getNumericValue = (lhr, auditName) =>
   (lhr.audits[auditName] && lhr.audits[auditName].numericValue) || NaN;
 
 /**
+ * @param {Array<number>} numbers
+ * @return {number}
+ */
+function getMedianValue(numbers) {
+  const sorted = numbers.slice().sort((a, b) => a - b);
+  if (sorted.length % 2 === 1) return sorted[(sorted.length - 1) / 2];
+  const lowerValue = sorted[sorted.length / 2 - 1];
+  const upperValue = sorted[sorted.length / 2];
+  return (lowerValue + upperValue) / 2;
+}
+
+/**
  * @param {LH.Result} lhr
  * @param {number} medianFcp
  * @param {number} medianInteractive
  */
 function getMedianSortValue(lhr, medianFcp, medianInteractive) {
-  const distanceFcp = medianFcp - getNumericValue(lhr, 'first-contentful-paint');
+  const distanceFcp =
+    medianFcp - getNumericValue(lhr, 'first-contentful-paint');
   const distanceInteractive =
     medianInteractive - getNumericValue(lhr, 'interactive');
 
@@ -23,49 +36,58 @@ function getMedianSortValue(lhr, medianFcp, medianInteractive) {
 }
 
 /**
- * We want the run that's closest to the median of the FCP and the median of the TTI. 
+ * We want the run that's closest to the median of the FCP and the median of the TTI.
  * We're using the Euclidean distance for that (https://en.wikipedia.org/wiki/Euclidean_distance).
+ * We use FCP and TTI because they represent the earliest and latest moments in the page lifecycle.
+ * We avoid the median of single measures like the performance score because they can still exhibit
+ * outlier behavior at the beginning or end of load.
+ *
  * @param {Array<LH.Result>} runs
- * @return {LH.Result|undefined}
+ * @return {LH.Result}
  */
 function computeMedianRun(runs) {
-  const validRuns = runs
-    .filter(run => Number.isFinite(getNumericValue(run, 'first-contentful-paint')))
-    .filter(run => Number.isFinite(getNumericValue(run, 'interactive')));
-  if (!validRuns.length) return undefined;
-
-  const sortedByFcp = validRuns
-    .slice()
-    .sort(
-      (a, b) =>
-        getNumericValue(a, 'first-contentful-paint') -
-        getNumericValue(b, 'first-contentful-paint')
-    );
-  const medianFcp = getNumericValue(
-    sortedByFcp[Math.floor(validRuns.length / 2)],
-    'first-contentful-paint'
+  const missingFcp = runs.some(run =>
+    Number.isNaN(getNumericValue(run, 'first-contentful-paint'))
+  );
+  const missingTti = runs.some(run =>
+    Number.isNaN(getNumericValue(run, 'interactive'))
   );
 
-  const sortedByInteractive = validRuns
-    .slice()
-    .sort(
-      (a, b) =>
-        getNumericValue(a, 'interactive') - getNumericValue(b, 'interactive')
-    );
-  const medianInteractive = getNumericValue(
-    sortedByInteractive[Math.floor(validRuns.length / 2)],
-    'interactive'
+  if (!runs.length) throw new Error('No runs provided');
+  if (missingFcp) throw new Error(`Some runs were missing an FCP value`);
+  if (missingTti) throw new Error(`Some runs were missing a TTI value`);
+
+  const medianFcp = getMedianValue(
+    runs.map(run => getNumericValue(run, 'first-contentful-paint'))
   );
 
-  const sortedByProximityToMedian = validRuns
+  const medianInteractive = getMedianValue(
+    runs.map(run => getNumericValue(run, 'interactive'))
+  );
+
+  // Sort by proximity to the medians, breaking ties with the minimum TTI.
+  const sortedByProximityToMedian = runs
     .slice()
     .sort(
       (a, b) =>
         getMedianSortValue(a, medianFcp, medianInteractive) -
-        getMedianSortValue(b, medianFcp, medianInteractive)
+          getMedianSortValue(b, medianFcp, medianInteractive) ||
+        getNumericValue(a, 'interactive') - getNumericValue(b, 'interactive')
     );
 
   return sortedByProximityToMedian[0];
 }
 
-module.exports = {computeMedianRun};
+/**
+ * @param {Array<LH.Result>} runs
+ * @return {Array<LH.Result>}
+ */
+function filterToValidRuns(runs) {
+  return runs
+    .filter(run =>
+      Number.isFinite(getNumericValue(run, 'first-contentful-paint'))
+    )
+    .filter(run => Number.isFinite(getNumericValue(run, 'interactive')));
+}
+
+module.exports = {computeMedianRun, filterToValidRuns};
--- lighthouse-core/test/lib/median-run-test.js
+++ lighthouse-core/test/lib/median-run-test.js
@@ -7,7 +7,7 @@
 
 /* eslint-env jest */
 
-const {computeMedianRun} = require('../../lib/median-run.js');
+const {computeMedianRun, filterToValidRuns} = require('../../lib/median-run.js');
 
 describe('Median Runs', () => {
   function lhr(auditNumericValues) {
@@ -19,56 +19,105 @@ describe('Median Runs', () => {
     return {audits};
   }
 
-  it('should pick the median run', () => {
-    const runs = [
-      lhr({'interactive': 100, 'first-contentful-paint': 100}),
-      lhr({'interactive': 200, 'first-contentful-paint': 200}),
-      lhr({'interactive': 300, 'first-contentful-paint': 300}),
-      lhr({'interactive': 400, 'first-contentful-paint': 400}),
-      lhr({'interactive': 500, 'first-contentful-paint': 500}),
-    ];
+  describe('computeMedianRun()', () => {
+    it('should pick the median run on an odd set', () => {
+      const runs = [
+        lhr({'interactive': 100, 'first-contentful-paint': 100}),
+        lhr({'interactive': 200, 'first-contentful-paint': 200}),
+        lhr({'interactive': 300, 'first-contentful-paint': 300}),
+        lhr({'interactive': 400, 'first-contentful-paint': 400}),
+        lhr({'interactive': 500, 'first-contentful-paint': 500}),
+      ];
 
-    expect(computeMedianRun(runs)).toEqual(runs[2]);
+      expect(computeMedianRun(runs)).toEqual(runs[2]);
+    });
+
+    it('should pick the median run on an even set', () => {
+      const runs = [
+        lhr({'interactive': 1000, 'first-contentful-paint': 400}),
+        lhr({'interactive': 2000, 'first-contentful-paint': 500}),
+        lhr({'interactive': 4000, 'first-contentful-paint': 200}),
+        lhr({'interactive': 5000, 'first-contentful-paint': 100}),
+      ];
+
+      expect(computeMedianRun(runs)).toEqual(runs[2]);
+    });
+
+    it('should pick the lower median run on an even set as tiebreaker', () => {
+      const runs = [
+        lhr({'interactive': 100, 'first-contentful-paint': 100}),
+        lhr({'interactive': 200, 'first-contentful-paint': 200}),
+        lhr({'interactive': 400, 'first-contentful-paint': 400}),
+        lhr({'interactive': 500, 'first-contentful-paint': 500}),
+      ];
+
+      expect(computeMedianRun(runs)).toEqual(runs[1]);
+    });
+
+    it('should avoid FCP outliers', () => {
+      const runs = [
+        lhr({'interactive': 100, 'first-contentful-paint': 100}),
+        lhr({'interactive': 250, 'first-contentful-paint': 400}),
+        lhr({'interactive': 300, 'first-contentful-paint': 10000}),
+        lhr({'interactive': 400, 'first-contentful-paint': 400}),
+        lhr({'interactive': 500, 'first-contentful-paint': 500}),
+      ];
+
+      expect(computeMedianRun(runs)).toEqual(runs[1]);
+    });
+
+    it('should avoid TTI outliers', () => {
+      const runs = [
+        lhr({'interactive': 100, 'first-contentful-paint': 100}),
+        lhr({'interactive': 200, 'first-contentful-paint': 200}),
+        lhr({'interactive': 10000, 'first-contentful-paint': 300}),
+        lhr({'interactive': 300, 'first-contentful-paint': 400}),
+        lhr({'interactive': 500, 'first-contentful-paint': 500}),
+      ];
+
+      expect(computeMedianRun(runs)).toEqual(runs[3]);
+    });
+
+    it('should throw on empty arrays', () => {
+      expect(() => computeMedianRun([])).toThrow();
+    });
+
+    it('should throw when missing FCP', () => {
+      expect(() => computeMedianRun([lhr({interactive: 500})])).toThrow();
+    });
+
+    it('should throw when missing TTI', () => {
+      expect(() => computeMedianRun([lhr({'first-contentful-paint': 500})])).toThrow();
+    });
+
+    it('should throw on lhrs with missing audits', () => {
+      const runs = [
+        lhr({'interactive': 100, 'first-contentful-paint': 100}),
+        lhr({'interactive': 200, 'first-contentful-paint': 200}),
+        lhr({'first-contentful-paint': 300}),
+        lhr({'interactive': 450}),
+        lhr({'interactive': 500, 'first-contentful-paint': 500}),
+      ];
+
+      expect(() => computeMedianRun(runs)).toThrow();
+    });
   });
 
-  it('should avoid FCP outliers', () => {
-    const runs = [
-      lhr({'interactive': 100, 'first-contentful-paint': 100}),
-      lhr({'interactive': 250, 'first-contentful-paint': 400}),
-      lhr({'interactive': 300, 'first-contentful-paint': 10000}),
-      lhr({'interactive': 400, 'first-contentful-paint': 400}),
-      lhr({'interactive': 500, 'first-contentful-paint': 500}),
-    ];
+  describe('filterToValidRuns()', () => {
+    it('should filter down to only runs with FCP and TTI', () => {
+      const runs = [
+        lhr({'interactive': 100, 'first-contentful-paint': 100}),
+        lhr({'interactive': 200, 'first-contentful-paint': 200}),
+        lhr({'first-contentful-paint': 300}),
+        lhr({'interactive': 450}),
+        lhr({'interactive': 500, 'first-contentful-paint': 500}),
+      ];
 
-    expect(computeMedianRun(runs)).toEqual(runs[1]);
-  });
-
-  it('should avoid TTI outliers', () => {
-    const runs = [
-      lhr({'interactive': 100, 'first-contentful-paint': 100}),
-      lhr({'interactive': 200, 'first-contentful-paint': 200}),
-      lhr({'interactive': 10000, 'first-contentful-paint': 300}),
-      lhr({'interactive': 300, 'first-contentful-paint': 400}),
-      lhr({'interactive': 500, 'first-contentful-paint': 500}),
-    ];
-
-    expect(computeMedianRun(runs)).toEqual(runs[3]);
-  });
-
-  it('should support empty arrays', () => {
-    expect(computeMedianRun([])).toEqual(undefined);
-    expect(computeMedianRun([lhr({'works-offline': 1})])).toEqual(undefined);
-  });
-
-  it('should support lhrs with missing audits', () => {
-    const runs = [
-      lhr({'interactive': 100, 'first-contentful-paint': 100}),
-      lhr({'interactive': 200, 'first-contentful-paint': 200}),
-      lhr({'first-contentful-paint': 300}),
-      lhr({'interactive': 450}),
-      lhr({'interactive': 500, 'first-contentful-paint': 500}),
-    ];
-
-    expect(computeMedianRun(runs)).toEqual(runs[1]);
+      expect(filterToValidRuns(runs)).toEqual([
+        runs[0],
+        runs[1],
+        runs[4],
+      ]);
+    });
   });
 });
