4579c865fa357c75c91a94a3893403038111a550
Patrick Hulce <patrick.hulce@gmail.com>
2020-05-28 14:09:34 -0500

    Apply suggestions from code review
    Co-authored-by: Brendan Kenny <bckenny@gmail.com>

--- lighthouse-core/lib/median-run.js
+++ lighthouse-core/lib/median-run.js
@@ -1,5 +1,5 @@
 /**
- * @license Copyright 2019 Google Inc. All Rights Reserved.
+ * @license Copyright 2020 Lighthouse Authors. All Rights Reserved.
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License. You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
  * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.
  */
@@ -24,7 +24,7 @@ function getMedianSortValue(lhr, medianFcp, medianInteractive) {
 
 /**
  * We want the run that's closest to the median of the FCP and the median of the TTI. 
- * We're using the squared distance for that (https://en.wikipedia.org/wiki/Euclidean_distance).
+ * We're using the Euclidean distance for that (https://en.wikipedia.org/wiki/Euclidean_distance).
  * @param {Array<LH.Result>} runs
  * @return {LH.Result|undefined}
  */
