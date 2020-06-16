bf612286532dd0eb0faa397c3718f8e5bc11e1f5
Connor Clark <cjamcl@google.com>
2020-06-16 16:01:25 -0700

    ts

--- lighthouse-core/report/html/renderer/performance-category-renderer.js
+++ lighthouse-core/report/html/renderer/performance-category-renderer.js
@@ -141,6 +141,7 @@ class PerformanceCategoryRenderer extends CategoryRenderer {
         value = audit.id === 'cumulative-layout-shift' ?
           clampTo2Decimals(audit.result.numericValue) :
           Math.round(audit.result.numericValue);
+        value = String(value);
       } else {
         value = 'null';
       }
