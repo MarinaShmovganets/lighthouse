c8b5c65f4f57ba03eb75d001efc949f1ecb1293f
Connor Clark <cjamcl@google.com>
2020-06-17 22:36:24 -0700

    wip

--- lighthouse-core/scripts/download-chrome-canary.sh
+++ lighthouse-core/scripts/download-chrome-canary.sh
@@ -8,6 +8,6 @@
 
 wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | apt-key add -
 sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list'
-apt-get update
-apt-get install -y google-chrome-canary --no-install-recommends
-apt-get clean
+sudo apt-get update
+sudo apt-get install -y google-chrome-canary --no-install-recommends
+sudo apt-get clean
