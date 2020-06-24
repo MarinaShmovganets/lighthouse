7dfc00d089163a7e145d1cf23c82f1fb620bff93
Paul Irish <paulirish@google.com>
2020-06-24 16:24:05 -0700

    misc: annotate version-specific logic with COMPAT comments

--- lighthouse-core/gather/driver.js
+++ lighthouse-core/gather/driver.js
@@ -1520,7 +1520,7 @@ class Driver {
   blockUrlPatterns(urls) {
     return this.sendCommand('Network.setBlockedURLs', {urls})
       .catch(err => {
-        // TODO: remove this handler once m59 hits stable
+        // TODO(COMPAT): remove this handler once m59 hits stable
         if (!/wasn't found/.test(err.message)) {
           throw err;
         }
