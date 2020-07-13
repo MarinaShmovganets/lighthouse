510992dbeb4d2043eb65730e7549bc86ee11091e
Adam Raine <ad.st.raine@gmail.com>
2020-07-13 13:41:03 -0400

    lint

--- lighthouse-core/test/audits/byte-efficiency/render-blocking-resources-test.js
+++ lighthouse-core/test/audits/byte-efficiency/render-blocking-resources-test.js
@@ -76,14 +76,14 @@ describe('Render blocking resources audit', () => {
     expect(result.numericValue).toMatchInlineSnapshot(`450`);
     expect(result.details.items).toMatchObject([
       {
-        "totalBytes": 621,
-        "url": "https://fonts.googleapis.com/css?family=Fira+Sans+Condensed%3A400%2C400i%2C600%2C600i&subset=latin%2Clatin-ext&display=swap",
-        "wastedMs": 440,
+        'totalBytes': 621,
+        'url': 'https://fonts.googleapis.com/css?family=Fira+Sans+Condensed%3A400%2C400i%2C600%2C600i&subset=latin%2Clatin-ext&display=swap',
+        'wastedMs': 440,
       },
       {
-        "totalBytes": 621,
-        "url": "https://fonts.googleapis.com/css?family=Montserrat",
-        "wastedMs": 440,
+        'totalBytes': 621,
+        'url': 'https://fonts.googleapis.com/css?family=Montserrat',
+        'wastedMs': 440,
       },
     ]);
   });
