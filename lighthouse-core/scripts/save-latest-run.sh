2a5690e48bfe5fc9aafdef51275b4bdc302acd19
Patrick Hulce <patrick.hulce@gmail.com>
2020-10-09 17:13:48 -0500

    Apply suggestions from code review
    Co-authored-by: Paul Irish <paulirish@google.com>

--- lighthouse-core/scripts/save-latest-run.sh
+++ lighthouse-core/scripts/save-latest-run.sh
@@ -9,11 +9,11 @@
 set -euo pipefail
 
 # Saves the necessary contents of the `latest-run/` folder to a subfolder for easier A/B comparison.
-# Restoring the contents to `latest-run/` is just `cp latest-run/latest-run.bak/* latest-run/`.
+# Restoring the contents to `latest-run/` is just `cp latest-run/latest-run-bak/* latest-run/`.
 
 DIRNAME="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 LH_ROOT="$DIRNAME/../.."
-TARGET_DIR=${1:-latest-run.bak}
+TARGET_DIR=${1:-latest-run-bak}
 
 cd "$LH_ROOT/latest-run"
 mkdir -p "$TARGET_DIR"
