8e28eafa47b6514a6237ed3434a5c2a670d6a3f3
Paul Irish <paulirish@google.com>
2020-11-20 14:15:38 -0800

    Revert "Revert "core(artifacts): encapsulate node details in an object (#11474)""
    This reverts commit 666057a819dd6a8daeae5652f97d6408819deb59.

--- lighthouse-core/gather/gatherers/image-elements.js
+++ lighthouse-core/gather/gatherers/image-elements.js
@@ -88,7 +88,7 @@ function getHTMLImages(allElements) {
       // https://html.spec.whatwg.org/multipage/images.html#pixel-density-descriptor
       usesSrcSetDensityDescriptor: / \d+(\.\d+)?x/.test(element.srcset),
       // @ts-expect-error - getNodeDetails put into scope via stringification
-      ...getNodeDetails(element),
+      node: getNodeDetails(element),
     };
   });
 }
@@ -139,7 +139,7 @@ function getCSSImages(allElements) {
       usesSrcSetDensityDescriptor: false,
       resourceSize: 0, // this will get overwritten below
       // @ts-expect-error - getNodeDetails put into scope via stringification
-      ...getNodeDetails(element),
+      node: getNodeDetails(element),
     });
   }
 
@@ -341,7 +341,7 @@ class ImageElements extends Gatherer {
       element.resourceSize = Math.min(resourceSize, transferSize);
 
       if (!element.isInShadowDOM && !element.isCss) {
-        await this.fetchSourceRules(driver, element.devtoolsNodePath, element);
+        await this.fetchSourceRules(driver, element.node.devtoolsNodePath, element);
       }
       // Images within `picture` behave strangely and natural size information isn't accurate,
       // CSS images have no natural size information at all. Try to get the actual size if we can.
