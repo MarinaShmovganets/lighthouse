668d04e4839872098d0f1d892e25c896d6c54722
Connor Clark <cjamcl@google.com>
2020-12-22 15:06:21 -0600

    w.e. async you can stay, silly jest

--- lighthouse-core/fraggle-rock/gather/driver.js
+++ lighthouse-core/fraggle-rock/gather/driver.js
@@ -51,7 +51,7 @@ class Driver {
    * @param {{useIsolation?: boolean}} [options]
    * @return {Promise<*>}
    */
-  evaluateAsync(expression, options) {
+  async evaluateAsync(expression, options) {
     if (!this._executionContext) throw new Error('Driver not connected to page');
     return this._executionContext.evaluateAsync(expression, options);
   }
