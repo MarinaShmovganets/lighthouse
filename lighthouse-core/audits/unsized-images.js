3aef0056bc416e59cd69239485bcfaf48bcf2d38
Paul Irish <paulirish@google.com>
2021-02-23 13:14:46 -0800

    address new code

--- lighthouse-core/audits/unsized-images.js
+++ lighthouse-core/audits/unsized-images.js
@@ -13,6 +13,7 @@
 const Audit = require('./audit.js');
 const i18n = require('./../lib/i18n/i18n.js');
 const URL = require('./../lib/url-shim.js');
+const { isGetAccessor } = require('typescript');
 
 const UIStrings = {
   /** Title of a Lighthouse audit that provides detail on whether all images have explicit width and height. This descriptive title is shown to users when every image has explicit width and height */
@@ -101,7 +102,9 @@ class UnsizedImages extends Audit {
    */
   static isNonNetworkSvg(image) {
     const isSvg = image.mimeType === 'image/svg+xml';
-    const isNonNetwork = URL.NON_NETWORK_PROTOCOLS.some(protocol => image.src.startsWith(protocol));
+    // Cheap test to avoid using the URL constuctor
+    const quickNDirtyProtocol = image.src.slice(0, image.src.indexOf(':'))
+    const isNonNetwork = URL.isNonNetworkProtocol(quickNDirtyProtocol);
     return isSvg && isNonNetwork;
   }
 
