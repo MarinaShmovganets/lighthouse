9ed8071933acdb4b1c1da896607f4ac34f846430
Connor Clark <cjamcl@google.com>
2021-04-30 17:35:11 -0600

    pr

--- lighthouse-cli/test/smokehouse/frontends/smokehouse-bin.js
+++ lighthouse-cli/test/smokehouse/frontends/smokehouse-bin.js
@@ -19,12 +19,7 @@ const log = require('lighthouse-logger');
 const {runSmokehouse} = require('../smokehouse.js');
 const {server, serverForOffline} = require('../../fixtures/static-server.js');
 
-let coreTestDefnsPath = '';
-try {
-  coreTestDefnsPath = require.resolve('../test-definitions/core-tests.js');
-} catch (_) {
-  // Won't exist in published package.
-}
+const coreTestDefnsPath = `${__dirname}/../test-definitions/core-tests.js`;
 
 /**
  * Possible Lighthouse runners. Loaded dynamically so e.g. a CLI run isn't
