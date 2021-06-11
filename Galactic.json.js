3d458bd4a530eba933da5d59b0538cfa8568ebbc
Michael Glenn <mbg198618@gmail.com>
2021-06-11 17:12:17 -0500

    Create Galactic.json.js

---
+++ Galactic.json.js
@@ -0,0 +1,478 @@
+{
+  "J0012+54": {
+    "dm": {
+      "display_name": "DM (pc/cc)",
+      "value": 131.3
+    },
+    "dec": {
+      "value": "+54:37:43",
+      "display_name": "Dec (deg)"
+    },
+    "ra": {
+      "value": "00:12:25",
+      "display_name": "RA (deg)"
+    },
+    "period": {
+      "value": 3.0248,
+      "error_low": 0.0008,
+      "error_high": 0.0008,
+      "display_name": "Spin Period (s)"
+    }
+  },
+  "J0121+5329": {
+    "ra": {
+      "value": "01:21:19",
+      "display_name": "RA (deg)"
+    },
+    "dec": {
+      "value": "53:29:24",
+      "display_name": "Dec (deg)"
+    },
+    "dm": {
+      "value": 87.35,
+      "display_name": "DM (pc/cc)"
+    },
+    "period": {
+      "display_name": "Spin Period (s)",
+      "value": "2.7255"
+    }
+  },
+  "J0209+58": {
+    "period": {
+      "display_name": "Spin Period (s)",
+      "value": "1.0637"
+    },
+    "dm": {
+      "display_name": "DM (pc/cc)",
+      "value": 56
+    },
+    "dec": {
+      "display_name": "Dec (deg)",
+      "value": "58:12:01"
+    },
+    "ra": {
+      "value": "02:09:12",
+      "display_name": "RA (deg)"
+    }
+  },
+  "J0227+33": {
+    "dec": {
+      "value": "+33:55:44",
+      "display_name": "Dec (deg)"
+    },
+    "dm": {
+      "display_name": "DM (pc/cc)",
+      "value": 27.56
+    },
+    "period": {
+      "display_name": "Spin Period (s)",
+      "value": 1.24
+    },
+    "ra": {
+      "value": "02:27:08",
+      "display_name": "RA (deg)"
+    }
+  },
+  "J0503+22": {
+    "period": {
+      "value": {},
+      "display_name": "Spin Period (s)"
+    },
+    "dec": {
+      "display_name": "Dec (deg)",
+      "value": "+22:20:00"
+    },
+    "ra": {
+      "value": "05:03:08",
+      "display_name": "RA (deg)"
+    },
+    "dm": {
+      "display_name": "DM (pc/cc)",
+      "value": 92
+    }
+  },
+  "J0658+29": {
+    "dm": {
+      "display_name": "DM (pc/cc)",
+      "value": 40.05
+    },
+    "dec": {
+      "display_name": "Dec (deg)",
+      "value": "+29:32:08"
+    },
+    "ra": {
+      "display_name": "RA (deg)",
+      "value": "06:58:06"
+    },
+    "period": {
+      "display_name": "Spin Period (s)",
+      "value": 0.82
+    }
+  },
+  "J0740+17": {
+    "dec": {
+      "value": "+17:03:02",
+      "display_name": "Dec (deg)"
+    },
+    "dm": {
+      "value": 42.98,
+      "display_name": "DM (pc/cc)"
+    },
+    "period": {
+      "value": {},
+      "display_name": "Spin Period (s)"
+    },
+    "ra": {
+      "value": "07:40:37",
+      "display_name": "RA (deg)"
+    }
+  },
+  "J0746+55": {
+    "dm": {
+      "display_name": "DM (pc/cc)",
+      "value": 10.5
+    },
+    "dec": {
+      "display_name": "Dec (deg)",
+      "value": "+55:12:00"
+    },
+    "period": {
+      "display_name": "Spin Period (s)",
+      "value": 2.89347
+    },
+    "ra": {
+      "display_name": "RA (deg)",
+      "value": "07:46:47"
+    }
+  },
+  "J0854+54": {
+    "ra": {
+      "value": "08:54:24",
+      "display_name": "RA (deg)"
+    },
+    "period": {
+      "value": "1.2329",
+      "display_name": "Spin Period (s)"
+    },
+    "dec": {
+      "value": "54:48:00",
+      "display_name": "Dec (deg)"
+    },
+    "dm": {
+      "value": 17.8,
+      "display_name": "DM (pc/cc)"
+    }
+  },
+  "J1048+53": {
+    "period": {
+      "value": {},
+      "display_name": "Spin Period (s)"
+    },
+    "ra": {
+      "display_name": "RA (deg)",
+      "value": "10:48:28"
+    },
+    "dm": {
+      "value": 30.85,
+      "display_name": "DM (pc/cc)"
+    },
+    "dec": {
+      "display_name": "Dec (deg)",
+      "value": "+53:41:49"
+    }
+  },
+  "J1106+02": {
+    "dm": {
+      "value": 16.4,
+      "display_name": "DM (pc/cc)"
+    },
+    "dec": {
+      "display_name": "Dec (deg)",
+      "value": "+2:28:50"
+    },
+    "ra": {
+      "value": "11:06:06.53",
+      "display_name": "RA (deg)"
+    },
+    "period": {
+      "display_name": "Spin Period (s)",
+      "value": 6.4
+    }
+  },
+  "J1130+09": {
+    "period": {
+      "display_name": "Spin Period (s)",
+      "value": {}
+    },
+    "dm": {
+      "display_name": "DM (pc/cc)",
+      "value": 22.4
+    },
+    "ra": {
+      "value": "11:31:12",
+      "display_name": "RA (deg)"
+    },
+    "dec": {
+      "value": "+09:25:48",
+      "display_name": "Dec (deg)"
+    }
+  },
+  "J1246+53": {
+    "dm": {
+      "display_name": "DM (pc/cc)",
+      "value": 21.03
+    },
+    "ra": {
+      "display_name": "RA (deg)",
+      "value": "12:51:48"
+    },
+    "period": {
+      "display_name": "Spin Period (s)",
+      "value": {}
+    },
+    "dec": {
+      "display_name": "Dec (deg)",
+      "value": "53:41:24"
+    }
+  },
+  "J1541+47": {
+    "period": {
+      "display_name": "Spin Period (s)",
+      "value": 0.277672
+    },
+    "dec": {
+      "value": "+47:03:06",
+      "display_name": "Dec (deg)"
+    },
+    "dm": {
+      "display_name": "DM (pc/cc)",
+      "value": 19.4
+    },
+    "ra": {
+      "value": "15:41:08",
+      "display_name": "RA (deg)"
+    }
+  },
+  "J1838+5051": {
+    "ra": {
+      "display_name": "RA (deg)",
+      "value": "18:38:01"
+    },
+    "dec": {
+      "value": "50:54:00",
+      "display_name": "Dec (deg)"
+    },
+    "dm": {
+      "value": 21.8,
+      "display_name": "DM (pc/cc)"
+    },
+    "period": {
+      "value": "2.5772",
+      "display_name": "Spin Period (s)"
+    }
+  },
+  "J1931+4229": {
+    "ra": {
+      "display_name": "RA (deg)",
+      "value": "19:30:55"
+    },
+    "dec": {
+      "display_name": "Dec (deg)",
+      "value": "42:40:12"
+    },
+    "period": {
+      "display_name": "Spin Period (s)",
+      "value": {}
+    },
+    "dm": {
+      "display_name": "DM (pc/cc)",
+      "value": 50.89
+    }
+  },
+  "J1943+58": {
+    "ra": {
+      "display_name": "RA (deg)",
+      "value": "19:43:00"
+    },
+    "dec": {
+      "value": "+58:13:00",
+      "display_name": "Dec (deg)"
+    },
+    "dm": {
+      "value": 71.2,
+      "display_name": "DM (pc/cc)"
+    },
+    "period": {
+      "display_name": "Spin Period (s)",
+      "value": 1.27085
+    }
+  },
+  "J2005+38": {
+    "period": {
+      "value": 2.462,
+      "display_name": "Spin Period (s)"
+    },
+    "dm": {
+      "display_name": "DM (pc/cc)",
+      "value": 192.8
+    },
+    "ra": {
+      "display_name": "RA (deg)",
+      "value": "20:05:12"
+    },
+    "dec": {
+      "value": "+38:44:21",
+      "display_name": "Dec (deg)"
+    }
+  },
+  "J2008+37": {
+    "period": {
+      "display_name": "Spin Period (s)",
+      "value": {}
+    },
+    "ra": {
+      "display_name": "RA (deg)",
+      "value": "20:08:05"
+    },
+    "dm": {
+      "value": 142.5,
+      "display_name": "DM (pc/cc)"
+    },
+    "dec": {
+      "value": "+37:56:46",
+      "display_name": "Dec (deg)"
+    }
+  },
+  "J2057+46": {
+    "ra": {
+      "display_name": "RA (deg)",
+      "value": "20:57:06"
+    },
+    "dec": {
+      "display_name": "Dec (deg)",
+      "value": "+46:54:58"
+    },
+    "period": {
+      "value": 0.559573,
+      "display_name": "Spin Period (s)"
+    },
+    "dm": {
+      "display_name": "DM (pc/cc)",
+      "value": 218.962
+    }
+  },
+  "J2108+45": {
+    "dm": {
+      "display_name": "DM (pc/cc)",
+      "value": 84
+    },
+    "period": {
+      "value": 0.577369,
+      "display_name": "Spin Period (s)"
+    },
+    "dec": {
+      "display_name": "Dec (deg)",
+      "value": "+45:18:00"
+    },
+    "ra": {
+      "value": "21:08:01",
+      "display_name": "RA (deg)"
+    }
+  },
+  "J2116+37": {
+    "dec": {
+      "value": "+37:07:45",
+      "display_name": "Dec (deg)"
+    },
+    "ra": {
+      "display_name": "RA (deg)",
+      "value": "21:16:20"
+    },
+    "dm": {
+      "value": 44,
+      "display_name": "DM (pc/cc)"
+    },
+    "period": {
+      "value": 0.145,
+      "display_name": "Spin Period (s)"
+    }
+  },
+  "J2208+46": {
+    "dm": {
+      "value": 63,
+      "display_name": "DM (pc/cc)"
+    },
+    "dec": {
+      "value": "+46:06:50",
+      "display_name": "Dec (deg)"
+    },
+    "ra": {
+      "value": "22:08:31",
+      "display_name": "RA (deg)"
+    },
+    "period": {
+      "display_name": "Spin Period (s)",
+      "value": 0.6425
+    }
+  },
+  "J2215+45": {
+    "dec": {
+      "value": "+45:20:25",
+      "display_name": "Dec (deg)"
+    },
+    "period": {
+      "display_name": "Spin Period (s)",
+      "value": 2.723
+    },
+    "ra": {
+      "value": "22:15:50",
+      "display_name": "RA (deg)"
+    },
+    "dm": {
+      "value": 18.5,
+      "display_name": "DM (pc/cc)"
+    }
+  },
+  "J2221+81": {
+    "dec": {
+      "value": "+81:24:56",
+      "display_name": "Dec (deg)"
+    },
+    "ra": {
+      "value": "22:21:40",
+      "display_name": "RA (deg)"
+    },
+    "dm": {
+      "error_high": 0.5,
+      "value": 38.5,
+      "display_name": "DM (pc/cc)",
+      "error_low": 0.5
+    },
+    "period": {
+      "display_name": "Spin Period (s)",
+      "value": {}
+    }
+  },
+  "J2355+15": {
+    "dm": {
+      "error_low": 0.5,
+      "error_high": 0.5,
+      "value": 26,
+      "display_name": "DM (pc/cc)"
+    },
+    "period": {
+      "error_high": 0.0005,
+      "error_low": 0.0005,
+      "display_name": "Spin Period (s)",
+      "value": 1.0964
+    },
+    "dec": {
+      "display_name": "Dec (deg)",
+      "value": "+15:30:00"
+    },
+    "ra": {
+      "display_name": "RA (deg)",
+      "value": "23:55:12"
+    }
+  }
+}
