32c2f1496f6acf2f722397042163b4e58f485508
Michael Glenn <mbg198618@gmail.com>
2021-07-01 12:04:54 -0500

    Create docs-brew-install-jq.js

---
+++ docs-brew-install-jq.js
@@ -0,0 +1,8 @@
+Docs
+Some of our docs have tests that run only in CI by defaul
+If you end up needing to modify our documentation, 
+  you'll need to run yarn test-docs locally to make sure they pass.
+
+Additional Dependencies
+
+brew install jq
