db63d00b6d7d8990aa4abbb9995516760985ac30
Michael Glenn <mbg198618@gmail.com>
2021-07-01 12:01:37 -0500

    Create test-lighthouse.js

---
+++ test-lighthouse.js
@@ -0,0 +1,14 @@
+# lint and test all files
+yarn test
+
+# watch for file changes and run tests
+#   Requires http://entrproject.org : brew install entr
+yarn watch
+
+## run linting, unit, and smoke tests separately
+yarn lint
+yarn unit
+yarn smoke
+
+## run tsc compiler
+yarn type-check
