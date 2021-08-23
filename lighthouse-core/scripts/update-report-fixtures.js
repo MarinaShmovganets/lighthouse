abdc7d2e08719e57ed12b688641fa2cb179af9bf
Connor Clark <cjamcl@google.com>
2021-08-23 15:25:24 -0700

    inject fake events

--- lighthouse-core/scripts/update-report-fixtures.js
+++ lighthouse-core/scripts/update-report-fixtures.js
@@ -5,16 +5,19 @@
  */
 'use strict';
 
+const fs = require('fs');
 const cli = require('../../lighthouse-cli/run.js');
 const cliFlags = require('../../lighthouse-cli/cli-flags.js');
 const assetSaver = require('../lib/asset-saver.js');
-const artifactPath = 'lighthouse-core/test/results/artifacts';
-
 const {server} = require('../../lighthouse-cli/test/fixtures/static-server.js');
 const budgetedConfig = require('../test/results/sample-config.js');
+const {LH_ROOT} = require('../../root.js');
 
-// All artifacts must have resources from a consistent port, to ensure reproducibility. https://github.com/GoogleChrome/lighthouse/issues/11776
+const artifactPath = 'lighthouse-core/test/results/artifacts';
+// All artifacts must have resources from a consistent port, to ensure reproducibility.
+// https://github.com/GoogleChrome/lighthouse/issues/11776
 const MAGIC_SERVER_PORT = 10200;
+
 /**
  * Update the report artifacts. If artifactName is set only that artifact will be updated.
  * @param {keyof LH.Artifacts=} artifactName
@@ -33,6 +36,8 @@ async function update(artifactName) {
   await cli.runLighthouse(url, flags, budgetedConfig);
   await server.close();
 
+  await augmentDefaultPassTrace();
+
   if (artifactName) {
     // Revert everything except the one artifact
     const newArtifacts = assetSaver.loadArtifacts(artifactPath);
@@ -45,6 +50,55 @@ async function update(artifactName) {
     finalArtifacts[artifactName] = newArtifact;
     await assetSaver.saveArtifacts(finalArtifacts, artifactPath);
   }
+}
+
+async function augmentDefaultPassTrace() {
+  const defaultPassTracePath = `${LH_ROOT}/${artifactPath}/defaultPass.trace.json`;
+  /** @type {LH.Trace} */
+  const traceData = JSON.parse(fs.readFileSync(defaultPassTracePath, 'utf-8'));
+  // Delete manually added events. Makes this function idempotent.
+  traceData.traceEvents = traceData.traceEvents.filter(e =>
+    // @ts-expect-error
+    !e._comment);
+  const markDomContentEventIndex =
+    traceData.traceEvents.findIndex((e) => e.name === 'MarkDOMContent');
+  const markDomContentEvent = traceData.traceEvents[markDomContentEventIndex];
+  if (!markDomContentEvent) throw new Error('could not find MarkDOMContent');
+
+  traceData.traceEvents.push({
+    // @ts-expect-error
+    '_comment': 'Manually added event to make sample lhr not error',
+    'name': 'largestContentfulPaint::Candidate',
+    'pid': markDomContentEvent.pid,
+    'tid': markDomContentEvent.tid,
+    'ts': markDomContentEvent.ts,
+    'ph': 'R',
+    'cat': 'loading,rail,devtools.timeline',
+    'args': {
+      'frame': markDomContentEvent.args.frame,
+      'data': {'size': 50},
+    },
+  });
+  traceData.traceEvents.push({
+    // @ts-expect-error
+    '_comment': 'Manually added event to make test CLS',
+    'name': 'LayoutShift',
+    'pid': markDomContentEvent.pid,
+    'tid': markDomContentEvent.tid,
+    'ts': markDomContentEvent.ts,
+    'ph': 'R',
+    'cat': 'loading,rail,devtools.timeline',
+    'args': {
+      'frame': markDomContentEvent.args.frame,
+      'data': {
+        'is_main_frame': true,
+        'score': 0.42,
+        'cumulative_score': 0.42,
+        'weighted_score_delta': 0.42,
+      },
+    },
+  });
+  await assetSaver.saveTrace(traceData, defaultPassTracePath);
 }
 
 update(/** @type {keyof LH.Artifacts | undefined} */ (process.argv[2]));
