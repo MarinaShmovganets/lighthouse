195863f7d2c2b4d3e30ad31fcfe1eeab41bbc2fb
Adam Raine <ad.st.raine@gmail.com>
2021-09-30 15:17:35 -0400

    tooltip strings

--- flow-report/src/i18n/ui-strings.js
+++ flow-report/src/i18n/ui-strings.js
@@ -13,6 +13,12 @@ export const UIStrings = {
   timespanDescription: 'User interactions',
   /** Description of a report that evaluates the state of a web page at a single point in time. */
   snapshotDescription: 'Captured state of page',
+  /** Label for a report that evaluates a page navigation. */
+  navigationReport: 'Navigation report',
+  /** Label for a report that evaluates a period of time where a user could have interacted with the page. */
+  timespanReport: 'Timespan report',
+  /** Label for a report that evaluates the state of a web page at a single point in time. */
+  snapshotReport: 'Snapshot report',
   /** Title of a home page that summarizes and links to the other pages. */
   summary: 'Summary',
   /** Title of a report section lists and links to multiple sub-reports. All caps is preferred but not required. */
@@ -23,4 +29,12 @@ export const UIStrings = {
   desktop: 'Desktop',
   /** Label for a report evaluating a web page. Label indicates that the report refers to the mobile version of the site. */
   mobile: 'Mobile',
+  /** Rating indicating that a report category is good/passing. */
+  ratingPass: 'Good',
+  /** Rating indicating that a report category is average or needs improvement. */
+  ratingAverage: 'Average',
+  /** Rating indicating that a report category is poor/failing. */
+  ratingFail: 'Poor',
+  /** Rating indicating that a report category rating could not be calculated because of an error. */
+  ratingError: 'Error',
 };
