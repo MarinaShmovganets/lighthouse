15a292b8b57e0c785a5ac3fa6d143bceb491e52d
Connor Clark <cjamcl@google.com>
2021-10-12 13:15:41 -0700

    update

--- build/rollup-brfs.js
+++ build/rollup-brfs.js
@@ -1,4 +1,3 @@
-// @ts-nocheck
 /**
  * @license Copyright 2021 The Lighthouse Authors. All Rights Reserved.
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License. You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
@@ -6,8 +5,13 @@
  */
 'use strict';
 
+// TODO: brfs doesn't work for ES modules, so this is a stopgap solution
+//       for the present usecases that aren't in esm yet. Will be replaced
+//       with a full-featured inlining plugin soon.
+
 const path = require('path');
 const {Readable} = require('stream');
+// @ts-expect-error - no types.
 const brfs = require('@wardpeet/brfs');
 
 const EXTENSIONS = ['.js', '.jsx', '.ts', '.tsx'];
@@ -15,6 +19,10 @@ const EXTENSIONS = ['.js', '.jsx', '.ts', '.tsx'];
 const rollupBrfs = function rollUpBrfs(options = {}) {
   return {
     name: 'brfs',
+    /**
+     * @param {string} code
+     * @param {string} id
+     */
     transform(code, id) {
       const ext = path.extname(id);
       if (!EXTENSIONS.includes(ext)) {
@@ -26,7 +34,7 @@ const rollupBrfs = function rollUpBrfs(options = {}) {
         src.push(code);
         src.push(null);
         const stream = src.pipe(brfs(id, options));
-        stream.on('data', function(data) {
+        stream.on('data', /** @param {Buffer} data */ function(data) {
           output += data.toString();
         });
         stream.on('end', function() {
@@ -35,7 +43,7 @@ const rollupBrfs = function rollUpBrfs(options = {}) {
             map: {mappings: ''},
           });
         });
-        stream.on('error', function(error) {
+        stream.on('error', /** @param {Error} error */ function(error) {
           reject(error);
         });
       });
