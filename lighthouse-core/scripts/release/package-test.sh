23061741441227ce13cea8ee4140da9db8a23558
Connor Clark <cjamcl@google.com>
2021-10-13 14:21:34 -0700

    pubads

--- lighthouse-core/scripts/release/package-test.sh
+++ lighthouse-core/scripts/release/package-test.sh
@@ -20,6 +20,7 @@ cd /tmp/lighthouse-local-test
 
 npm init -y
 npm install "$LH_ROOT/lighthouse-$VERSION.tgz"
+npm install lighthouse-plugin-publisher-ads
 npm explore lighthouse -- npm run fast -- http://example.com
 
 # Packaged smokehouse/lighthouse using root's static-server and test fixtures.
