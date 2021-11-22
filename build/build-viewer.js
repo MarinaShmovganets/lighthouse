98dc70c2d3f4df615717b913a6631c67c955c9c6
Connor Clark <cjamcl@google.com>
2021-11-22 13:37:14 -0800

    flag

--- build/build-viewer.js
+++ build/build-viewer.js
@@ -19,7 +19,7 @@ async function buildReportGenerator() {
       }),
       rollupPlugins.commonjs(),
       rollupPlugins.nodeResolve(),
-      rollupPlugins.inlineFs({verbose: false}),
+      rollupPlugins.inlineFs({verbose: Boolean(process.env.DEBUG)}),
     ],
   });
 
