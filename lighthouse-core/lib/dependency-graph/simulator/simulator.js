34de6aadcd48bb2459f801917d60a4d1af68c62c
Adam Raine <ad.st.raine@gmail.com>
2021-12-09 17:14:49 -0500

    comment

--- lighthouse-core/lib/dependency-graph/simulator/simulator.js
+++ lighthouse-core/lib/dependency-graph/simulator/simulator.js
@@ -493,7 +493,8 @@ class Simulator {
     const {throughput, observedThroughput} = this._options;
 
     // https://github.com/GoogleChrome/lighthouse/pull/13323#issuecomment-962031709
-    // 0 throughput means the throttling is unset. Use the observed throughput in this case.
+    // 0 throughput means the no (additional) throttling is expected.
+    // This is common for desktop + devtools throttling where throttling is additive and we don't want any additional.
     const bitsPerSecond = throughput || observedThroughput;
     if (bitsPerSecond === 0) return 0;
 
