15d120c383be9a0fa1e1193d1b6089a55d051528
Paul Irish <paulirish@google.com>
2022-04-19 13:50:34 -0700

    hoisted

--- report/test-assets/faux-psi.js
+++ report/test-assets/faux-psi.js
@@ -60,6 +60,8 @@ async function renderLHReport() {
           metaItemsEl,
           reportRootEl.querySelector('.lh-buttons')
         );
+        reportRootEl.querySelector('.lh-metrics-container')?.parentElement?.classList
+            .add('lh--hoisted-meta');
       }
 
       container.append(reportRootEl);
