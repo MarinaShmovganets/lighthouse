31e40fc2a3c81a8e6d70e4bdb5fae60cf1d945d6
Connor Clark <cjamcl@google.com>
2022-09-02 13:14:36 -0700

    ws

--- core/audits/byte-efficiency/legacy-javascript.js
+++ core/audits/byte-efficiency/legacy-javascript.js
@@ -4,7 +4,6 @@
  * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.
  */
 
-
 /**
  * @fileoverview Identifies polyfills and transforms that should not be present if using module/nomodule pattern.
  * @see https://docs.google.com/document/d/1ItjJwAd6e0Ts6yMbvh8TN3BBh_sAd58rYE1whnpuxaA/edit Design document
--- core/audits/third-party-facades.js
+++ core/audits/third-party-facades.js
@@ -4,7 +4,6 @@
  * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.
  */
 
-
 /**
  * @fileoverview Audit which identifies third-party code on the page which can be lazy loaded.
  * The audit will recommend a facade alternative which is used to imitate the third-party resource until it is needed.
