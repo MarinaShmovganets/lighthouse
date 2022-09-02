73fb5a7b5808d1fa15fd443b2d775e44a84a1f47
Connor Clark <cjamcl@google.com>
2022-09-02 13:17:02 -0700

    ws

--- core/gather/gatherers/trace.js
+++ core/gather/gatherers/trace.js
@@ -4,7 +4,6 @@
  * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.
  */
 
-
 /**
  * @fileoverview
  * This gatherer collects all network and page devtools protocol traffic during the timespan/navigation.
--- core/lib/navigation-error.js
+++ core/lib/navigation-error.js
@@ -4,7 +4,6 @@
  * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.
  */
 
-
 import {LighthouseError} from './lh-error.js';
 import {NetworkAnalyzer} from './dependency-graph/simulator/network-analyzer.js';
 import {NetworkRequest} from './network-request.js';
