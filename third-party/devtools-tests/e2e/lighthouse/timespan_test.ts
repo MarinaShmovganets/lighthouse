baf62516ee68f6bd8dd7fe9a7f66890dd7436bc7
Adam Raine <ad.st.raine@gmail.com>
2022-12-01 14:30:54 -0800

    all modes

--- third-party/devtools-tests/e2e/lighthouse/timespan_test.ts
+++ third-party/devtools-tests/e2e/lighthouse/timespan_test.ts
@@ -30,6 +30,9 @@ describe('Timespan', async function() {
   this.timeout(60_000);
 
   beforeEach(() => {
+    // https://github.com/GoogleChrome/lighthouse/issues/14572
+    expectError(/Request CacheStorage\.requestCacheNames failed/);
+
     // https://bugs.chromium.org/p/chromium/issues/detail?id=1357791
     expectError(/Protocol Error: the message with wrong session id/);
     expectError(/Protocol Error: the message with wrong session id/);
