dee67378c1e2bd58c0195f5d0f560c8c0dba91c1
Adam Raine <ad.st.raine@gmail.com>
2023-06-28 15:43:52 -0400

    no skip

--- core/test/legacy/config/config-test.js
+++ core/test/legacy/config/config-test.js
@@ -23,7 +23,7 @@ const require = createRequire(import.meta.url);
 const moduleDir = getModuleDirectory(import.meta);
 const modulePath = getModulePath(import.meta);
 
-describe.skip('Config', () => {
+describe('Config', () => {
   let origConfig;
   beforeEach(() => {
     origConfig = JSON.parse(JSON.stringify(legacyDefaultConfig));
