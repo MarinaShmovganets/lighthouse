aba675456eafd89d266fe312c820849835180da2
Adam Raine <ad.st.raine@gmail.com>
2023-07-07 11:00:56 -0700

    comment

--- cli/test/smokehouse/test-definitions/a11y.js
+++ cli/test/smokehouse/test-definitions/a11y.js
@@ -859,6 +859,7 @@ const expectations = {
                 'type': 'node',
                 'selector': 'body > section > span#target-size-2',
                 'snippet': '<span role="button" tabindex="0" id="target-size-2">',
+                // Exact target size can vary depending on the device.
                 'explanation': /^Fix any of the following:\n {2}Target has insufficient size \([0-9]+px by [0-9]+px, should be at least 24px by 24px\)\n {2}Target has insufficient offset from its closest neighbor \([0-9]+px should be at least 24px\)$/,
                 'nodeLabel': 'o',
               },
