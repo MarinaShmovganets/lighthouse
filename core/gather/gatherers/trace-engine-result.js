ad7ea6aa6da1ee6433b292005a16a919a5a29070
Connor Clark <cjamcl@google.com>
2023-12-19 16:32:02 -0800

    better comment

--- core/gather/gatherers/trace-engine-result.js
+++ core/gather/gatherers/trace-engine-result.js
@@ -53,15 +53,16 @@ class TraceEngineResult extends BaseGatherer {
           return response.node;
         } catch (err) {
           if (err.message.includes('Could not find node with given id')) {
-            // TODO: when injecting an iframe, the engine gets the node of that frame's document element,
-            // which happens to be a backend id of 0. But we can't tell when the nodeId is from the non-main
-            // session... so ignore it for now.
+            // TODO: when injecting an iframe, the engine gets the node of that frame's document element.
+            // But we don't have a way to access that frame. We just have our default session.
             // Ex:
             // node cli http://localhost:10503/shift-attribution.html --quiet --only-audits layout-shifts
             // To fix we must:
             // - Change trace engine `getNode` protocol interface to also give frame id
             // - Expand our driver.targetManager to know how to talk to a session connected to a specific frame
             // When this is fixed, remove this try/catch.
+            // Note: this could be buggy by giving the wrong node detail if a node id meant for a non-main frame
+            // happens to match one from the main frame ... which is pretty likely...
             return null;
           }
           throw err;
