23997cad5e53018481f6d2f49eb80af0817c5842
Connor Clark <cjamcl@google.com>
2023-12-21 14:05:48 -0800

    make order not matter

--- cli/test/smokehouse/test-definitions/shift-attribution.js
+++ cli/test/smokehouse/test-definitions/shift-attribution.js
@@ -13,23 +13,25 @@ const expectations = {
     finalDisplayedUrl: 'http://localhost:10200/shift-attribution.html',
     audits: {
       'layout-shifts': {
-        score: 0,
         details: {
-          items: [
-            {
-              node: {selector: 'body > div#blue'},
-              subItems: {items: [{cause: /font/, extra: {value: /Regular\.ttf/}}]},
-            },
-            {
-              node: {selector: 'body > div#blue'},
-              // TODO: We can't get nodes from non-main frame yet. See runRootCauseAnalysis.
-              subItems: {items: [{cause: /iframe/, extra: undefined}]},
-            },
-            {
-              node: {selector: 'body > div#blue'},
-              subItems: {items: [{cause: /Media/, extra: {selector: 'body > img'}}]},
-            },
-          ],
+          items: {
+            // Order (aka shift size) may vary due to environment.
+            _includes: [
+              {
+                node: {selector: 'body > div#blue'},
+                subItems: {items: [{cause: /font/, extra: {value: /Regular\.ttf/}}]},
+              },
+              {
+                node: {selector: 'body > div#blue'},
+                // TODO: We can't get nodes from non-main frames yet. See runRootCauseAnalysis.
+                subItems: {items: [{cause: /iframe/, extra: undefined}]},
+              },
+              {
+                node: {selector: 'body > div#blue'},
+                subItems: {items: [{cause: /Media/, extra: {selector: 'body > img'}}]},
+              },
+            ],
+          },
         },
       },
     },
