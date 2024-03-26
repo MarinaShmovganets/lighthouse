57bbcb58792e1cc48c5fe8e8117897ead13cb21a
Connor Clark <cjamcl@google.com>
2024-03-26 09:40:27 -0700

    misc: use "processing duration" instead of "processing time" for INP

--- core/test/audits/work-during-interaction-test.js
+++ core/test/audits/work-during-interaction-test.js
@@ -173,8 +173,8 @@ Object {
           },
           Object {
             "phase": Object {
-              "formattedDefault": "Processing time",
-              "i18nId": "core/audits/work-during-interaction.js | processingTime",
+              "formattedDefault": "Processing duration",
+              "i18nId": "core/audits/work-during-interaction.js | processingDuration",
               "values": undefined,
             },
             "subItems": Object {
