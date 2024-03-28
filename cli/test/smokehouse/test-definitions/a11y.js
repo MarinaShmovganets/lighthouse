42574a85e3cdb2126e865437e5674a563618fa38
Adam Raine <ad.st.raine@gmail.com>
2024-03-28 10:56:42 -0700

    core: remove `duplicate-id-aria` and `duplicate-id-active`

--- cli/test/smokehouse/test-definitions/a11y.js
+++ cli/test/smokehouse/test-definitions/a11y.js
@@ -452,37 +452,6 @@ const expectations = {
 
         },
       },
-      'duplicate-id-active': {
-        score: 0,
-        details: {
-          items: [
-            {
-              node: {
-                'type': 'node',
-                'selector': 'body > section > textarea#duplicate-id-active',
-                'path': '2,HTML,1,BODY,47,SECTION,0,TEXTAREA',
-                'snippet': '<textarea id="duplicate-id-active" aria-label="text1">',
-                'explanation': 'Fix any of the following:\n  Document has active elements with the same id attribute: duplicate-id-active',
-                'nodeLabel': 'text1',
-              },
-              subItems: {
-                type: 'subitems',
-                items: [
-                  {
-                    relatedNode: {
-                      'type': 'node',
-                      'path': '2,HTML,1,BODY,47,SECTION,1,TEXTAREA',
-                      'selector': 'body > section > textarea#duplicate-id-active',
-                      'snippet': '<textarea id="duplicate-id-active" aria-label="text2">',
-                      'nodeLabel': 'text2',
-                    },
-                  },
-                ],
-              },
-            },
-          ],
-        },
-      },
       'duplicate-id-aria': {
         score: 1,
         details: {
