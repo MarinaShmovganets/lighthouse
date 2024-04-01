be73c6e9d2733ccc35706a465317dfef689225f1
Connor Clark <cjamcl@google.com>
2024-04-01 13:31:14 -0700

    ok

--- core/computed/page-dependency-graph.js
+++ core/computed/page-dependency-graph.js
@@ -30,9 +30,9 @@ class PageDependencyGraph {
     // Calculates the URL artifact from the processed trace and DT log.
     const URL = data.URL || await DocumentUrls.request(data, context);
 
-    const traceEvents = processedTrace.mainThreadEvents;
+    const mainThreadEvents = processedTrace.mainThreadEvents;
     const lanternRequests = networkRecords.map(NetworkRequest.asLanternNetworkRequest);
-    return LanternPageDependencyGraph.createGraph(traceEvents, lanternRequests, URL);
+    return LanternPageDependencyGraph.createGraph(mainThreadEvents, lanternRequests, URL);
   }
 }
 
